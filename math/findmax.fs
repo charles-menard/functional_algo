cceac57461eac32f1995ed686a21e3b7aed289bc
killerzdouou <charles.menard@live.fr>
2018-01-30 14:09:32 -0500

    find max works but without input

--- math/findmax.fs
+++ math/findmax.fs
@@ -1,22 +1,35 @@
+open System
 let average a b =
     (a+b) /2.0
 
 let derive f (dx : float)=
-    fun x -> (f (x + dx) + f x)/ dx
+    fun x -> (f (x + dx) - f x)/ dx
 
-let closeenough a b (tolerance:float) =
-    abs(a-b) < tolerance
 
+
+//given a function, and a point a where the derivative is >0
+//a point b where it is <0 and tolerancethis f approximates a local max of f
+// (the dx is arbitrary
 let rec findMax f (a:float) (b:float) tolerance =
     let midpoint = average a b
     let derivative =
-        (derive f 0.000001) midpoint
+        (derive f 0.0001) midpoint
     match abs(derivative)< tolerance with
     |true -> midpoint
     |false ->
         match derivative with
-        |n when n>0.0 ->
-            findMax f midpoint b
-        |n when n<0.0 ->
-            findMax f a midpoint
-	|_ -> midpoint
+        |n when n > 0.0 ->
+            findMax f midpoint b tolerance
+        |n when n < 0.0 ->
+            findMax f a midpoint tolerance
+        |_ -> midpoint
+
+
+
+let main() =
+    //Console.Write("What's your name? ")
+    //let name = Console.ReadLine()
+    let max = findMax (fun x -> sin x) 0.0 2.0 0.0001
+    printfn "The max is situated at %f" max
+    
+main()
